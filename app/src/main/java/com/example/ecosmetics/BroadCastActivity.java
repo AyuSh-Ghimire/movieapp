0a9e4695f8844038cd1b70602a614d68711c537d
sadina-jarga-magar <sadinajmagar1999@gmail.com>
2020-01-29 09:01:32 +0545

    checking connectivity on process

---
+++ app/src/main/java/com/example/ecosmetics/BroadCastActivity.java
@@ -0,0 +1,31 @@
+package com.example.ecosmetics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+import com.example.ecosmetics.Broadcast.BroadCastReceiver;
+
+public class BroadCastActivity extends AppCompatActivity {
+BroadCastReceiver broadCastReceiver= new BroadCastReceiver();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(broadCastReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadCastReceiver);
+    }
+}
